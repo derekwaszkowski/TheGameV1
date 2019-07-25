ea735a4f5c79685fa40ce850b13107570f9e39ef
dwaszkowski <derekwaszkowski@gmail.com>
2019-07-25 17:05:33 -0400

    v1.1.1

--- app/src/main/java/com/example/thegame/FlyingFishView.java
+++ app/src/main/java/com/example/thegame/FlyingFishView.java
@@ -2,6 +2,7 @@ package com.example.thegame;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
